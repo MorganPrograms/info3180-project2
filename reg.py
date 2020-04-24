98ad2152b3f38e8dd7a7eae74bf8c271ac5078d4
MoniqueCampbell <43890502+MoniqueCampbell@users.noreply.github.com>
2020-04-24 18:47:06 -0500

    Form

--- reg.py
+++ reg.py
@@ -4,12 +4,12 @@ from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import InputRequired, DataRequired, Email
 
 class MyForm(FlaskForm):
-	username = StringField('Username', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     location = StringField('Location', validators=[DataRequired()])
     biography = TextAreaField('Biography', render_kw={"rows": 5, "cols": 40}, validators=[DataRequired()])
-	photo = FileField('Photo', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'Images only!'])])
+    photo = FileField('Photo', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'Images only!'])])
     
