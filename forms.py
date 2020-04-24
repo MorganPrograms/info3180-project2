9f3aadfef71ec401be54b72678807d10bca788e4
MoniqueCampbell <43890502+MoniqueCampbell@users.noreply.github.com>
2020-04-24 18:54:51 -0500

    Login Form

--- forms.py
+++ forms.py
@@ -1,9 +1,7 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from wtforms import TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired
 
-class UploadForm(FlaskForm):
-      description = TextAreaField('Description', validators=[DataRequired()])
-      photo = FileField('Photo', validators=[FileRequired(),FileAllowed(['jpg', 'png', 'Images only!'])])
-    
+class Login(FlaskForm):
+	username = StringField('Username', validators=[InputRequired()])
+      password = PasswordField('Password', validators=[InputRequired()])
